292f92314fb51c305a49f433d1d34e204a8a6c7b
Ekaterina Kovergina <ekaterina.kovergina@idp.zyfra.com>
2022-04-05 18:47:28 +0300

    add some functionatity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-reddit';
+
+  articles: Article[]
+
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'http://angular.io', 10),
+      new Article('Angular', 'http://angular.io', 12),
+      new Article('Angular', 'http://angular.io', 13),
+    ]
+  }
+
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link ${link.value}`)
+    this.articles.push(new Article(title.value, link.value, 0))
+    title.value = ''
+    link.value = ''
+    return false
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes)
+  }
 }
